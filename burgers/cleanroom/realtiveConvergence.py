7c4434c7a7a46756bcdb933c01b95611c2061353
Julian Quick <jaq8@humboldt.edu>
2018-03-30 13:17:21 -0600

    checking in

--- burgers/cleanroom/realtiveConvergence.py
+++ burgers/cleanroom/realtiveConvergence.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from fd import d1
 
-TOL = 0.01
+TOL = 0.002
 
 def ulinf(u, dx): return np.max(np.abs(u))
 def dulinf(u, dx): return np.max(np.abs(d1(u, dx)))
@@ -12,21 +12,34 @@ def dul2(u, dx): return np.sum(d1(u, dx) ** 2) / u.size
 def ul1(u, dx): return np.sum(np.abs(u)) / u.size
 def dul1(u, dx): return np.sum(np.abs(d1(u, dx))) / u.size
 
+qoiname = 'ul1'
 QOIFS = [ulinf, dulinf, ul2, dul2, ul1, dul1]
 QOINames = [r"$L_\infty(u)$", r"$L_\infty(\nabla u)$", r"$L_2(u)$", r"$L_2(\nabla u)",
             r"L_1(u)", r"L_1(\nabla u)"]
+fbig, axbig = plt.subplots()
+axbig.set_xscale('log')
+axbig.set_ylabel(qoiname)
+axbig.set_xlabel(r"$\nu$")
 #nus = np.array([1])
-nus = np.array([1, .1, .05, .04, .03, .02, .01, .005])
-#nus = np.array([1, .5, .3, .1, .05, .04, .03, 0.02, .015, .01, .005])
-f = open('xofnu001.csv', 'w')
+#nus = np.array([1, .8, .6, .4, .3, .2, .1, .05, .04, .03, .02, .015, .013, .011, .01, .008, .006, .005, .004, .002, .001, .0008, .0006, .0004, .0002])
+nus = np.array([1.0, 0.8, 0.6, 0.4, 0.3, 0.2, 0.1, 0.05, 0.04, 0.03, 0.02, 0.01, 0.005])
+fl = open('xofnu_%s_%f.csv'%(qoiname, TOL), 'w')
+fl.write("nu,nx,qoi\n")
+qs = []
+xs = []
 for nu in nus:
     lastqoi = None
     largestx = 0
     nx = 25
+    xs.append([])
+    qs.append([])
     while True:
         x = np.linspace(0, xf, nx) ; dx = x[1] - x[0]
-        u = geturec(nu=nu, x=x, convstrategy='4c', diffstrategy='5c', timestrategy='rk', evolution_time=2., n_save_t=20, ubl=0, ubr=0)
-        qoi = np.array([QOIFS[kk](u[:, -1], dx) for kk in range(len(QOIFS))])
+        u = geturec(nu=nu, x=x, convstrategy='4c', diffstrategy='5c', timestrategy='rk', evolution_time=1, n_save_t=20, ubl=0, ubr=0)
+        qoi = ul1(u[:, -1], dx)
+        xs[-1].append(nx)
+        qs[-1].append(qoi)
+        #qoi = np.array([QOIFS[kk](u[:, -1], dx) for kk in range(len(QOIFS))])
         if lastqoi is not None:
             print('nu: ', nu, ',  nx: ', nx, ",  QOI: ", QOINames, qoi, ",  res: ", (qoi - lastqoi) / qoi)
             #if not np.isnan(np.max(lastqoi)) and np.max(np.log(np.abs(qoi / lastqoi)) / np.log(2) ) < TOL: break
@@ -35,6 +48,13 @@ for nu in nus:
         nx *= 2
     if nx > largestx: 
          largestx = nx
-    plt.plot(u) ; plt.savefig("%f_ff01.pdf"%nu) ; plt.clf()
-    f.write(str(nu) + ', ' + str(largestx) +', '+' '.join([str(s) for s in qoi]) + "\n")
-f.close()
+    f, ax = plt.subplots()
+    ax.plot(u) ; f.savefig("%f_%s_ff01.pdf"%(nu, qoiname)) ; f.clf()
+    axbig.scatter(np.ones(len(qs[-1]))*nu, qs[-1], marker='x', c=xs[-1], cmap=plt.cm.plasma) 
+    fbig.savefig("conv_01_%s.pdf"%(qoiname)) 
+    fl.write(str(nu) + ', ' + str(largestx) +', '+ str(qoi) + "\n")
+    #fl.write(str(nu) + ', ' + str(largestx) +', '+' '.join([str(s) for s in qoi]) + "\n")
+axbig.plot(nus, [qs[ii][-1] for ii in range(len(nus))])
+fl.close()
+fbig.savefig("conv_01_%s.pdf"%(qoiname))
+plt.clf()
--- burgers/cleanroom/secondsolver.py
+++ burgers/cleanroom/secondsolver.py
@@ -36,8 +36,13 @@ def convective_dudt(un, dx, strategy='4c'):
 
     # Dirchlet BCs, 4-term-central convective
     elif strategy == '4c':
-        duconv[1] = -1 * un[1] * (un[2] - un[0]) / (2 * dx)
-        duconv[-2] = -1 * un[-2] * (un[-1] - un[-3]) / (2 * dx)
+
+        # http://web.media.mit.edu/~crtaylor/calculator.html
+        duconv[1] = -1 * un[1] * (-10 * un[0] - 77 * un[1] + 150 * un[2] - 100 * un[3] + 50 * un[4] -15 * un[5] + 2 * un[6]) / 60 / dx
+        duconv[-2] = -1 * un[-2] * (10 * un[-1] + 77 * un[-2] - 150 * un[-3] + 100 * un[-4] - 50 * un[-5] + 15 * un[-6] - 2 * un[6]) / 60 / dx
+
+        #duconv[1] = -1 * un[1] * (un[2] - un[0]) / (2 * dx)
+        #duconv[-2] = -1 * un[-2] * (un[-1] - un[-3]) / (2 * dx)
         duconv[2:-2] = -1 * un[2:-2] * (-1 * un[4:] + 8 * un[3:-1]  - 8 * un[1:-3] + un[:-4]) / ( 12 * dx)
     else: raise(IOError("Invalid convective strategy")) ; quit()
     return duconv
@@ -58,19 +63,26 @@ def diffusive_dudt(un, nu, dx, strategy='5c'):
         #print("!!!!!")
 
     elif strategy == '5c':
-        dundiff[1] = nu * (un[2] - 2 * un[1] + un[0]) / dx**2
-        dundiff[-2] = nu * (un[-1] - 2 * un[-2] + un[-3]) / dx**2
+        #dundiff[1] = nu * (un[2] - 2 * un[1] + un[0]) / dx**2
+        #dundiff[-2] = nu * (un[-1] - 2 * un[-2] + un[-3]) / dx**2
+
+        # http://web.media.mit.edu/~crtaylor/calculator.html
+        dundiff[1] = nu * (137 * un[0] - 147 * un[1] - 255 * un[2] + 470 * un[3] - 285 * un[4] + 93 * un[5] - 13 * un[6]) / 180 / dx**2
+        dundiff[-2] = nu * (137 * un[-1] - 147 * un[-2] - 255 * un[-3] + 470 * un[-4] - 285 * un[-5] + 93 * un[-6] - 13 * un[-7]) / (180 * dx**2)
         dundiff[2:-2] = nu * (-1 * un[4:] + 16 * un[3:-1] - 30 * un[2:-2] + 16 * un[1:-3] - un[:-4]) / (12 * dx**2 )
     else: raise(IOError("Invalid diffusive strategy")) ; quit()
     return dundiff
 
 @nb.jit
-def geturec(nu=.05, x=x, evolution_time=evolution_time, u0=None, n_save_t=500, ubl=1., ubr=1., convstrategy="4c", diffstrategy='5c', timestrategy='rk'):
+def geturec(nu=.05, x=x, evolution_time=evolution_time, u0=None, n_save_t=500, ubl=1., ubr=1., convstrategy="4c", diffstrategy='5c', timestrategy='rk', dt=None, returndt=False):
 
     # Prescribde cfl and cfts=.05
     dx = x[1] - x[0]
     #dt = min(.3 * dx / 3., .3 / nu * dx ** 2)
-    dt = min(.1 * dx / 20., .3 / nu * dx ** 2)
+    if dt is not None: pass
+    else: dt = min(.1 * dx / 20., .3 / nu * dx ** 2)
+    if returndt: return dt
+    print('dt is ', dt)
 
     #dt = min(.05 * dx / 20., .05 / nu * dx ** 2)
     #print(dt, dx, nu) ; hey
@@ -79,7 +91,7 @@ def geturec(nu=.05, x=x, evolution_time=evolution_time, u0=None, n_save_t=500, u
     if divider ==0: raise(IOError("not enough time steps to save %i times"%n_save_t))
 
     # initially purturb u
-    u_initial = ubl + .8 * np.sin(x * np.pi)
+    u_initial = ubl + np.sin(x * np.pi)
     if u0 is not None: u_initial = u0
     #u_initial = 1 + .2 * np.sin(x) #+ 0.3 * np.sin(x * 3)
     u = u_initial
@@ -115,7 +127,9 @@ def geturec(nu=.05, x=x, evolution_time=evolution_time, u0=None, n_save_t=500, u
         # scheme is to compute du/dt in two parts: convective and diffusive.
 
         # Combine convective and diffusive dudt terms to get full derivative
-        dudt = duconv + dudiff
+        dudt = dudiff # DANGER ZONE TODO DONT DO THIS
+        #dudt = duconv + dudiff
+
         #print('*********')
         #print(dudt)
         #print('====')
@@ -130,6 +144,7 @@ def geturec(nu=.05, x=x, evolution_time=evolution_time, u0=None, n_save_t=500, u
             uhalfn = un + dt * dudt / 2.
             duhalfn_dt = convective_dudt(uhalfn, dx, strategy=convstrategy) + diffusive_dudt(uhalfn, nu, dx, strategy=diffstrategy)
             u = un + 0.5 * dt * (dudt + duhalfn_dt)
+        #elif timestrategy == 'rk4':
         else: raise(IOError("Bad Strategy"))
 
         # Save every xth time step
